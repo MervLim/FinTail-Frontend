569ba172d1efa6090f70e1993987cae0075468ca
happypill <judolew@gmail.com>
2017-07-24 12:59:53 +0800

    Added News Axios Action

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -17,21 +17,43 @@ export const displayResult = (result) => {
   };
 }
 
+export const displayNewsResult = (news) => {
+  return {
+    type: 'DISPLAY_NEWS',
+    news
+  };
+}
+
+
 
 export const getStock = (searchTerm) => {
   console.log('im inside getStock actions')
   return (dispatch) => {
-<<<<<<< HEAD
-    axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=O4S8KZ7QADUOH6CX')
-=======
     console.log('im in axios dispatch');
     axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+searchTerm+'&apikey=O4S8KZ7QADUOH6CX')
->>>>>>> 29af197340e23b5ad9ae06e29f95377aba578ccf
       .then( (response) => {
         let stock = [];
         stock.push(response.data);
         // console.log(stock);
         dispatch(displayResult(stock));
+      })
+      .catch((error)=> {
+        console.error("AJAX: Could not get stock @ AlphaVantage")
+
+      });
+  };
+}
+
+export const getNews = (searchTerm) => {
+  console.log('im inside getStock actions')
+  return (dispatch) => {
+    console.log('im in axios dispatch');
+    axios.get(' https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=54c0d4afc52b4332b33762e86b37052d')
+      .then( (response) => {
+        let news= [];
+        news.push(response.data);
+        // console.log(stock);
+        dispatch(displayNewsResult(news));
       })
       .catch((error)=> {
         console.error("AJAX: Could not get stock @ AlphaVantage")
